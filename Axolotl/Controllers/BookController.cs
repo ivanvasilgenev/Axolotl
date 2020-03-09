34a57fd8e510f286ca3c45083d4dca814468c98a
Ivan-Vasil Genev <ivanvasiltgenev@gmail.com>
2020-03-09 14:29:30 +0200

    Add BookController Add Book model Add DB migrations

---
+++ Axolotl/Controllers/BookController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Axolotl.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Axolotl/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Axolotl.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public string Genre { get; set; }
+    }
+}
